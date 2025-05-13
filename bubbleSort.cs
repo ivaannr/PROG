f2765d7c6f92dc3ac4a909d528ad87e8451dda69
iván <128229332+ivaannr@users.noreply.github.com>
2025-05-13 13:57:42 +0200

    Create bubbleSort.cs

---
+++ bubbleSort.cs
@@ -0,0 +1,27 @@
+static void BubbleSort(int[] array)
+    {
+        int n = array.Length;
+        bool intercambiado;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            intercambiado = false;
+
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (array[j] > array[j + 1])
+                {
+                    
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                    intercambiado = true;
+                }
+            }
+
+           
+            if (!intercambiado)
+                break;
+        }
+    }
